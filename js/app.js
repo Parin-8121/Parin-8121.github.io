9e9dbbaea3fb026a93fa262c6cf62197e2a1c369
Parin Patel <parin.patel@triosstudent.com>
2025-04-09 19:47:54 -0400

    project2 added

--- js/app.js
+++ js/app.js
@@ -30,3 +30,52 @@ window.addEventListener('scroll', () => {
     navbar.classList.remove('scrolled');
   }
 });
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+  e.preventDefault();
+  const form = this;
+  const responseEl = document.getElementById('form-response');
+
+  fetch(form.action, {
+    method: form.method,
+    body: new FormData(form),
+    headers: { 'Accept': 'application/json' }
+  })
+  .then(response => {
+    if (response.ok) {
+      responseEl.classList.add('show');
+      form.reset();
+    } else {
+      responseEl.innerText = "Oops! Something went wrong.";
+      responseEl.classList.add('show');
+    }
+  })
+  .catch(error => {
+    responseEl.innerText = "Error sending message.";
+    responseEl.classList.add('show');
+  });
+});
+const body = document.body;
+const toggleBtn = document.getElementById("theme-toggle-btn");
+const themeIcon = document.getElementById("theme-icon");
+const themeLabel = document.getElementById("theme-label");
+
+// Initial load
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-theme");
+  themeIcon.src = "assets/images/icons/dark.png";
+  themeLabel.textContent = "Dark Mode";
+} else {
+  themeIcon.src = "assets/images/icons/light.png";
+  themeLabel.textContent = "Light Mode";
+}
+
+// On click
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-theme");
+
+  const isDark = body.classList.contains("dark-theme");
+
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+  themeIcon.src = isDark ? "assets/images/icons/dark.png" : "assets/images/icons/light.png";
+  themeLabel.textContent = isDark ? "Dark Mode" : "Light Mode";
+});
